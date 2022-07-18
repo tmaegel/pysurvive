de0e1f444ed8d82ea84e4a8f0e0b67d5e6b982b7
Toni Mägel <tmaegel@posteo.de>
2022-07-18 21:04:47 +0200

    Add script to easy create a tileset from a list of files

---
+++ make_tileset.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+import sys
+
+import pygame as pg
+from pygame.locals import SRCALPHA
+
+from pysurvive.utils import load_image
+
+COLORKEY = (255, 0, 255)
+
+
+def load_tile_from_filelist(
+    files_path: list[str], tile_size: int
+) -> list[list[pg.Surface]]:
+    tile_table = []
+
+    if not os.path.isdir(files_path):
+        print("Directory %s doesnt exists." % files_path)
+        sys.exit(1)
+
+    path, _, files = next(os.walk(files_path))
+    for img in sorted(files):
+        if not img.endswith(".png") or img.startswith("tilelist"):
+            continue
+
+        print("Proccessing file", img)
+        tile_image, tile_rect = load_image(f"{path}/{img}", alpha=True)
+        if tile_rect.width != tile_size or tile_rect.height != tile_size:
+            print(
+                "ERROR: The size of the tiles are not equal to the specific tile size."
+            )
+            sys.exit(1)
+        tile_table.append(tile_image)
+
+    return tile_table
+
+
+def main():
+    """
+    Script expect 2 positional arguments:
+        * 1. The path of the list of tiles (files).
+        * 2. The tile size in pixel.
+    """
+    args = sys.argv[1:]
+    try:
+        tileset_path = args[0]
+        tile_size = int(args[1])
+    except IndexError:
+        print("ERROR: Invalid number of arguments.")
+        sys.exit(1)
+
+    tileset_table = load_tile_from_filelist(tileset_path, tile_size)
+    tileset_image = pg.Surface(
+        (tile_size * len(tileset_table), tile_size), flags=SRCALPHA
+    )
+
+    for x, tile_img in enumerate(tileset_table):
+        tileset_image.blit(
+            tile_img,
+            (x * tile_size, 0),
+        )
+    pg.image.save(tileset_image, f"{tileset_path}/tilelist.png")
+
+
+if __name__ == "__main__":
+    pg.init()
+    _ = pg.display.set_mode((800, 600), pg.HIDDEN)
+    main()
